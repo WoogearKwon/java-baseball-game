af066f9413ca1f63764dd484e6814316c8142cc7
Woogear Kwon <techkwon1717@hanmail.net>
2021-07-05 00:35:49 +0900

    학습 테스트 실습 코드 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +11,35 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] numbers = "1,2".split(",");
+        assertThat(numbers).contains("1", "2");
+        assertThat(numbers).containsExactly("1", "2");
+    }
+
+    @Test
+    void subString() {
+        String sub = "(1,2)".substring(1,3);
+        assertThat(sub).isEqualTo(sub);
+    }
+
+    @Test
+    void charAt() {
+        String actual = "abc";
+        assertThatThrownBy(() -> {
+           // ..
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("Index: 2, Size 2");
+    }
+
+    /*
+    자주 발생하는 Exception의 경우 예외별 메서드 제공
+
+    * assertThatIllegalArgumentException()
+    * assertThatIllegalStateException()
+    * assertThatIOException()
+    * assertThatNullPointerException()
+    * */
 }
