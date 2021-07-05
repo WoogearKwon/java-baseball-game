b6a0d88078b53c73ee647ae25993febc1cb21620
Woogear Kwon <techkwon1717@hanmail.net>
2021-07-05 23:04:45 +0900

    ParameterizedTest 케이스 추가

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,8 +1,8 @@
 package study;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -17,21 +17,26 @@ public class SetTest {
     void setUp() {
         numbers = new HashSet<>();
         numbers.add(1);
-        numbers.add(1);
         numbers.add(2);
         numbers.add(3);
-    }
-
-    @Test
-    void contains() {
-        assertThat(numbers.contains(1)).isTrue();
-        assertThat(numbers.contains(2)).isTrue();
-        assertThat(numbers.contains(3)).isTrue();
+        numbers.add(4);
+        numbers.add(5);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"", "  "})
-    void isBlank_ShouldReturnTrueForNullOrBlackStrings(String input) {
-        
+    @ValueSource(ints = {1, 2, 3})
+    void containsNumbers(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, 4", "2, 5", "3, 5"})
+    void containsNumbersReturnTrueOrFalse(int small, int big) {
+//        assertThat(numbers.contains(small)).isTrue();
+//        assertThat(numbers.contains(big)).isFalse();
+
+        // 이게 답이 아닌 것 같은데, 잘 모르겠다..
+        if (numbers.contains(small)) assertThat(true);
+        assertThat(false);
     }
 }
