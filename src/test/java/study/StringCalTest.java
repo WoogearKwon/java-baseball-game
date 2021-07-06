aa43a296a71b5e7a33e9b928f41cc7209ebb72df
Woogear Kwon <techkwon1717@hanmail.net>
2021-07-06 17:57:23 +0900

    StringCalTest 리팩토링

--- src/test/java/study/StringCalTest.java
+++ src/test/java/study/StringCalTest.java
@@ -1,43 +1,51 @@
 package study;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalTest {
+    private int actual = -1;
     // String value = scanner.nextLine();
 
     @DisplayName("문자열 계산기")
-    @CsvSource(value = {"2 + 3 * 4 / 2, 10"})
+    @CsvSource(value = {"2 + 4 * 3 / 6, 3"})
     @ParameterizedTest
     void cal(String input, int expected) {
         String[] array = input.split(" ");
 
-        int actual = -1;
-
         for (int i = 0; i < array.length; i++) {
-
-            if (!isItSign(array[i])) {
-                if (actual == -1) {
-                    actual = Integer.parseInt(array[i]);
-                } else {
-                    actual = process(actual, Integer.parseInt(array[i]), array[i - 1]);
-                }
-            }
+            process(i, array);
         }
 
         System.out.println("result = " + actual);
         assertThat(expected == actual).isTrue();
     }
 
-    private boolean isItSign(String text) {
-        return text.equals("+") || text.equals("-") || text.equals("*") || text.equals("/");
+    private void process(int i, String[] array) {
+        if (shouldProcess(i)) {
+            setActualValue(i, array);
+        }
     }
 
-    private int process(int previous, int current, String sign) {
+    private void setActualValue(int i, String[] array) {
+        if (actual == -1) {
+            actual = Integer.parseInt(array[i]);
+            return;
+        }
+
+        int currentValue = Integer.parseInt(array[i]);
+        String mathSign = array[i - 1];
+        actual = calculate(actual, currentValue, mathSign);
+    }
+
+    private boolean shouldProcess(int i) {
+        return (i % 2 == 0);
+    }
+
+    private int calculate(int previous, int current, String sign) {
         if (sign.equals("+")) {
             return previous + current;
         }
